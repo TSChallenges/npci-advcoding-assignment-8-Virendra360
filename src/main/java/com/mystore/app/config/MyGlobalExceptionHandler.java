bb07bedd5d864ee1f2a65768a475aa29613f234c
Virendra <viru03062000@gmail.com>
2025-03-17 22:42:40 +0530

    Completed the assignment

--- src/main/java/com/mystore/app/config/MyGlobalExceptionHandler.java
+++ src/main/java/com/mystore/app/config/MyGlobalExceptionHandler.java
@@ -2,19 +2,21 @@ package com.mystore.app.config;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class MyGlobalExceptionHandler {
 
-    // TODO
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 }
